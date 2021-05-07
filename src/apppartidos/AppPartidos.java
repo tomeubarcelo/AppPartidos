e7ce1fda1036d188a7f208a9685834426ddcca3e
Tomeu Barcelo <tomeu.95@gmail.com>
2021-05-08 01:02:39 +0200

    prueba de conexion a bbdd y muestra resultados

--- src/apppartidos/AppPartidos.java
+++ src/apppartidos/AppPartidos.java
@@ -5,6 +5,11 @@
  */
 package apppartidos;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author tomeu
@@ -16,8 +21,24 @@ public class AppPartidos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
+        Connection con;
+        Statement stmt;
+        ResultSet rs;
+       
+        try {
+            con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1","system","Mallorca-107");
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("select * from partidos");
+            while (rs.next()) {                
+                System.out.println(rs.getString("local") + " "+ rs.getString("golsLocal") + " vs "+rs.getString("visitant")+ " " + rs.getString("golsVisitant"));
+            }
+            stmt.close();
+            rs.close();
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("SQLException: "+e.getMessage());
+        }
     }
     
 }
