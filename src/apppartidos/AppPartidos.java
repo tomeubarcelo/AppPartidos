a5eb4b2cae977f02e682d49c561a4827e18bcf05
Tomeu Barcelo <tomeu.95@gmail.com>
2021-05-08 10:46:05 +0200

    crear menu para la app y sus 3 opciones

--- src/apppartidos/AppPartidos.java
+++ src/apppartidos/AppPartidos.java
@@ -10,6 +10,7 @@ import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 /**
  *
  * @author tomeu
@@ -24,15 +25,50 @@ public class AppPartidos {
         
         Connection con;
         Statement stmt;
-        ResultSet rs;
+        ResultSet rs = null;
         Partido partido;
         try {
             con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1","system","Mallorca-107");
+            System.out.println("Connexio realitzada!");
+            
+            //crear sentencia
             stmt = con.createStatement();
-            rs = stmt.executeQuery("select local, visitant, golsLocal, golsVisitant, p.guanyador(local, golsLocal, visitant, golsVisitant) from partidos p");
-            while (rs.next()) {                
-                System.out.println(rs.getString(1) + " "+ rs.getString(3) + " vs "+rs.getString(4)+ " " + rs.getString(2)+ " -> " + rs.getString(5)  );
-            }
+            
+            byte opcio = 0;
+            //bucle para el menu 
+            
+            do {                
+               opcio = menuOpcions();
+                switch (opcio) {
+                    case 1: 
+                        //1a execucio
+                        System.out.println("Introducir datos en tabla");
+                        break;
+                        
+                    case 2:
+                        //2A execucio
+                        System.out.println("Mostrar partidos de la tabla");
+                        rs = stmt.executeQuery("select local, visitant, golsLocal, golsVisitant, p.guanyador(local, golsLocal, visitant, golsVisitant) from partidos p");
+                        while (rs.next()) {                
+                            System.out.println(rs.getString(1) + " "+ rs.getString(3) + " vs "+rs.getString(4)+ " " + rs.getString(2)+ " -> " + rs.getString(5)  );
+                        }
+                        
+                        break;
+                        
+                    case 3:
+                        System.out.println("Generar clasificacion");
+                        //3A execucio
+                        break;
+                        
+                    case 4:
+                        System.out.println("PROGRAMA FINALITZAT!!!");
+                        break;
+                    default:
+                        System.out.println("Aquesta opció no existeix.");
+                } 
+            }while (opcio==1 || opcio == 2 || opcio==3 );
+            
+
             
             //insert
             /*partido = new Partido ("Atletico de Madrid", 2, "Sevilla", 2);
@@ -45,9 +81,36 @@ public class AppPartidos {
             stmt.close();
             rs.close();
             con.close();
-        } catch (SQLException e) {
+            
+        }catch (SQLException e) {
             System.out.println("SQLException: "+e.getMessage());
-        }
+        } 
     }
+    
+    
+        //MENÚ DE OPCIONS
+    private static byte menuOpcions ()  {
+        byte opcio=0;
+        do{
+            try{
+                Scanner op = new Scanner (System.in);
+                //menú d'opcions del programa
+                System.out.println("1. Introducir datos en la tabla partidos.");
+                System.out.println("2. Mostrar los partidos de la tabla con el resultado y el ganador o ”empate” usando la función de la BD.");
+                System.out.println("3. Generar con las estructuras de java una clasificación obtenida a partir de la tabla partidos ordenada con el equipo con más puntos en primera posición (3 puntos por victoria, 1 punto por empate y 0 puntos por los partidos perdidos).");  
+                System.out.println("4. Salir de la aplicación");
+                System.out.println("Introdueix l'opcio elegida: ");
+                opcio=op.nextByte();
+                if (opcio < 1 || opcio > 4) {
+                System.out.println("Escollir entre (1..4)!.");    
+                }
+            }    
+            catch(Exception e){
+                System.out.println(e.getMessage()+ " . Error al llegir del teclat(1..4)!.");
+            }
+            
+        }while (opcio < 1 || opcio > 4);
+        return opcio;
+    } 
     
 }
